9f6c7b3999ffdeeea33e34d7b367de41ef3707ec
Melanietorales <melanie_torales@hotmail.com>
2025-05-04 14:58:28 -0300

    deleted unused package

--- demo/src/main/java/com/example/demo/client/PeopleClient.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.client;
-
-import com.example.demo.model.PeopleListResponse;
-
-public interface PeopleClient {
-    PeopleListResponse getPeopleData(int page, int limit, String name);
-}
--- demo/src/main/java/com/example/demo/service/PeopleService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.DTO.PeopleListResponseDto;
-
-public interface PeopleService {
-    PeopleListResponseDto getPeopleData(int page, int limit, String name);
-}
